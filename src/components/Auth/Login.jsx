cfc0d8e5b20e9036e2290a18c36a938e7281418b
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-16 14:48:00 +0530

    added minimum password length to be 8

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -27,7 +27,9 @@ const Login = () => {
     email: Yup.string()
       .email("Invalid email format")
       .required("Email is required"),
-    password: Yup.string().required("Password is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
   });
 
   const handleSubmit = async (values, { setSubmitting, setErrors }) => {
