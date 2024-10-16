134defcf39d24c390a0ce1baaae32855550b8692
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-16 13:23:10 +0530

    added icons in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext.jsx";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
@@ -41,10 +45,15 @@ const Navbar = () => {
                 component={Link}
                 to="/create"
                 sx={{ color: "#E0E1DD", marginRight: 2 }}
+                startIcon={<AddCircleIcon />}
               >
                 Create Post
               </Button>
-              <Button onClick={logout} sx={{ color: "#E0E1DD" }}>
+              <Button
+                onClick={logout}
+                sx={{ color: "#E0E1DD" }}
+                startIcon={<LogoutIcon />}
+              >
                 Logout
               </Button>
             </>
@@ -54,10 +63,16 @@ const Navbar = () => {
                 component={Link}
                 to="/login"
                 sx={{ color: "#E0E1DD", marginRight: 2 }}
+                startIcon={<LoginIcon />}
               >
                 Login
               </Button>
-              <Button component={Link} to="/register" sx={{ color: "#E0E1DD" }}>
+              <Button
+                component={Link}
+                to="/register"
+                sx={{ color: "#E0E1DD" }}
+                startIcon={<AppRegistrationIcon />}
+              >
                 Register
               </Button>
             </>
